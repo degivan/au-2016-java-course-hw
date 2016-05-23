25e02e2f76b03dd54c15f8b0a287ca4dec45b093
degivan <id1997@yandex.ru>
2016-05-23 10:35:44 +0300

    both parts

---
+++ src/main/java/spbau/mit/divan/practice/collectors/FirstPartTasks.java
@@ -0,0 +1,121 @@
+package spbau.mit.divan.practice.collectors;
+
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+/**
+ * Created by Degtjarenko Ivan on 21.05.2016.
+ */
+public class FirstPartTasks {
+
+    private FirstPartTasks() {
+    }
+
+    // Список названий альбомов
+    public static List<String> allNames(Stream<Album> albums) {
+        return albums
+                .map(Album::getName)
+                .collect(Collectors.toList());
+    }
+
+    // Список названий альбомов, отсортированный лексикографически по названию
+    public static List<String> allNamesSorted(Stream<Album> albums) {
+        return albums
+                .map(Album::getName)
+                .sorted()
+                .collect(Collectors.toList());
+    }
+
+    // Список треков, отсортированный лексикографически по названию, включающий все треки альбомов из 'albums'
+    public static List<String> allTracksSorted(Stream<Album> albums) {
+        return albums
+                .flatMap(album -> album
+                        .getTracks()
+                        .stream()
+                        .map(Track::getName))
+                .sorted()
+                .collect(Collectors.toList());
+    }
+
+    private final static int RATING_BOUND = 95;
+
+    // Список альбомов, в которых есть хотя бы один трек с рейтингом более 95, отсортированный по названию
+    public static List<Album> sortedFavorites(Stream<Album> s) {
+        return s
+                .filter(album -> album
+                        .getTracks()
+                        .stream()
+                        .anyMatch(track -> track.getRating() > RATING_BOUND))
+                .sorted((a1, a2) -> a1.getName().compareTo(a2.getName()))
+                .collect(Collectors.toList());
+    }
+
+    // Сгруппировать альбомы по артистам
+    public static Map<Artist, List<Album>> groupByArtist(Stream<Album> albums) {
+        return albums
+                .collect(Collectors.groupingBy(Album::getArtist));
+    }
+
+    // Сгруппировать альбомы по артистам (в качестве значения вместо объекта 'spbau.mit.divan.practice.collectors.Artist' использовать его имя)
+    public static Map<Artist, List<String>> groupByArtistMapName(Stream<Album> albums) {
+        return albums
+                .collect(Collectors.groupingBy(
+                        Album::getArtist,
+                        Collectors.mapping(
+                                Album::getName,
+                                Collectors.toList())));
+    }
+
+    // Число повторяющихся альбомов в потоке
+    public static long countAlbumDuplicates(Stream<Album> albums) {
+        return albums
+                .collect(Collectors.groupingBy(
+                        Album::getName, Collectors.counting()
+                ))
+                .values()
+                .stream()
+                .collect(Collectors.summingLong(a -> a - 1));
+    }
+
+    // Альбом в котором максимум рейтинга минимален
+    // (если в альбоме нет ни одного трека, считать, что максимум рейтинга в нем --- 0)
+    public static Optional<Album> minMaxRating(Stream<Album> albums) {
+        return albums.collect(Collectors.minBy(
+                Comparator.comparingInt(a -> a.getTracks()
+                        .stream()
+                        .map(Track::getRating)
+                        .max(Integer::compareTo)
+                        .orElse(0))
+        ));
+    }
+
+    // Список альбомов, отсортированный по убыванию среднего рейтинга его треков (0, если треков нет)
+    public static List<Album> sortByAverageRating(Stream<Album> albums) {
+        return albums
+                .sorted(Comparator.comparingDouble(a -> -a.getTracks()
+                        .stream()
+                        .collect(Collectors.averagingInt(Track::getRating))))
+                .collect(Collectors.toList());
+    }
+
+    // Произведение всех чисел потока по модулю 'modulo'
+    // (все числа от 0 до 10000)
+    public static int moduloProduction(IntStream stream, int modulo) {
+        return stream.reduce(1, (a, b) -> ((a % modulo) * (b % modulo)) % modulo);
+    }
+
+    // Вернуть строку, состояющую из конкатенаций переданного массива, и окруженную строками "<", ">"
+    // см. тесты
+    public static String joinTo(String... strings) {
+        return Arrays.asList(strings)
+                .stream()
+                .collect(Collectors.joining(", ", "<", ">"));
+    }
+
+    // Вернуть поток из объектов класса 'clazz'
+    public static <R> Stream<R> filterIsInstance(Stream<?> s, Class<R> clazz) {
+        return (Stream<R>) s.filter(clazz::isInstance);
+    }
+}
